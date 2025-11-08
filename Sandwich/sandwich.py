a2f7c55f9c883dea1a07641e3a79be7308019fde
João Victor <100379860+Joao-Victor-Developer@users.noreply.github.com>
2025-11-07 21:47:14 -0300

    Add files via upload

--- Sandwich/sandwich.py
+++ Sandwich/sandwich.py
@@ -1,11 +1,11 @@
 pilha = []
 
 while True:
-    print("\n--- Montagem do Sanduíche ---")
+  
     print("1 - Adicionar ingrediente")
-    print("2 - Remover ingrediente (do topo)")
-    print("3 - Ver último ingrediente adicionado")
-    print("4 - Mostrar sanduíche completo")
+    print("2 - Remover ingrediente ")
+    print("3 - Ver ingrediente adicionado")
+    print("4 - Mostrar sanduíche ")
     print("5 - Finalizar pedido")
 
     opcao = input("Escolha uma opção: ")
@@ -13,33 +13,33 @@ while True:
     if opcao == "1":
         ingrediente = input("Digite o nome do ingrediente: ")
         pilha.append(ingrediente)
-        print(f"Ingrediente '{ingrediente}' adicionado ao sanduíche.")
+        print(f"Ingrediente '{ingrediente}' adicionado.")
 
     elif opcao == "2":
         if len(pilha) > 0:
             removido = pilha.pop()
-            print(f"Ingrediente '{removido}' removido do topo.")
+            print(f"Ingrediente '{removido}' removido")
         else:
-            print("O sanduíche está vazio! Nada para remover.")
+            print("O sanduíche está vazio")
 
     elif opcao == "3":
         if len(pilha) > 0:
-            print(f"O último ingrediente adicionado foi: '{pilha[-1]}'")
+            print(f"O ingrediente adicionado foi: '{pilha[-1]}'")
         else:
-            print("O sanduíche ainda está vazio!")
+            print("O sanduíche ainda está sem")
 
     elif opcao == "4":
         if len(pilha) > 0:
-            print("\n--- Seu sanduíche ---")
+            print("--- Seu sanduíche ---")
             for ingrediente in pilha:
                 print(f"- {ingrediente}")
-            print("---------------------")
+           
         else:
-            print("Nenhum ingrediente no sanduíche.")
+            print("Nenhum ingrediente")
 
     elif opcao == "5":
-        print("Pedido finalizado! Bom apetite!")
+        print("Pedido finalizado")
         break
 
     else:
-        print("Opção inválida. Tente novamente.")
+        print("Opção inválida")
